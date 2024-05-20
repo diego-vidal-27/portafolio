af5dea870cb39f78ceb47705a60aa3586d737e64
diego-vidal-27 <149233639+diego-vidal-27@users.noreply.github.com>
2024-05-20 10:00:44 -0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,29 +1,25 @@
 <?php
-// Datos de conexión a la base de datos
 $servername = "localhost";
-$username = "id22185463_root"; // Tu nombre de usuario de MySQL
-$password = "123456Sistemas/"; // Tu contraseña de MySQL
-$database = "id22185463_bd_contactos"; // El nombre de tu base de datos
+$username = "id22185463_root"; 
+$password = "123456Sistemas/"; 
+$database = "id22185463_bd_contactos"; 
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Recibir los datos del formulario y validarlos
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
-// Preparar la sentencia SQL con una sentencia preparada
+
 $sql = "INSERT INTO contactos (nombre, email, telefono, mensaje) VALUES (?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
 
-// Vincular parámetros y ejecutar la sentencia
 $stmt->bind_param("ssss", $name, $email, $phone, $message);
 
 if ($stmt->execute()) {
@@ -32,7 +28,6 @@ if ($stmt->execute()) {
     echo "Error al enviar el mensaje: " . $stmt->error;
 }
 
-// Cerrar la conexión y liberar recursos
 $stmt->close();
 $conn->close();
 ?>
